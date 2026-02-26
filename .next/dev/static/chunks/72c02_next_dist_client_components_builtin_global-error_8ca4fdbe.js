8317e256ccd6f58ccea8d7a619a1c4b0b3c92548
Wagner <wagnerismohenrique@gmail.com>
2026-02-26 12:07:48 -0300

    Remove unused development files and type definitions from Next.js build

--- .next/dev/static/chunks/72c02_next_dist_client_components_builtin_global-error_8ca4fdbe.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/72c02_next_dist_43d47d5e._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/dyad-apps_radiant-penguin-crawl_src_app_layout_tsx_8ca4fdbe._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__e92cf196._.css",
-  "static/chunks/76bf5__pnpm_b74fb629._.js",
-  "static/chunks/dyad-apps_radiant-penguin-crawl_src_components_ui_sonner_tsx_97ee8a8d._.js"
-],
-    source: "dynamic"
-});
